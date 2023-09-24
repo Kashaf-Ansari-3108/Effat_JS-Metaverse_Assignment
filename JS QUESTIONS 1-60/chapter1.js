a4c8b1ac3419dfeabe027f1975f00838aec70a04
Kashaf-Ansari-3108 <kashafansari3108@gmail.com>
2023-09-24 19:35:04 +0500

    JS Questions 1-60

---
+++ JS QUESTIONS 1-60/chapter1.js
@@ -0,0 +1,12 @@
+//Question 1
+alert("Effat");
+alert("Ansari");
+alert("effatansari177@gmail.com");
+alert("03112561669");
+alert("@ngel17");
+
+//Question 2
+alert("You're learning JavaScript!");
+
+//Question 3
+alert("My Name is Effat Ansari");
---
+++ JS QUESTIONS 1-60/chapter2.js
@@ -0,0 +1,14 @@
+//q1
+let myVar;
+
+//q2
+let myName;
+myName="Effat";
+
+//q3
+const teamName="Champions";
+alert(teamName);
+
+//q4
+var bestMan = "Charlie";
+bestMan="John";
